64b8bd22d1e107a6303e1c3c7eb0e67d6e6eba8b
mehrinwagle <mehrinwagle26@gmail.com>
2025-08-20 16:03:24 +0530

    apps added

---
+++ ab/home/views.py
@@ -0,0 +1,7 @@
+from django.shortcuts import render
+from.models import Home
+
+def home(request):
+    data=Home.objects.all()
+    return render(request,'index.html',{'data':data})
+# Create your views here.
